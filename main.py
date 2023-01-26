843e6ff55f0566da7682435152ed65ecafde2d24
Anahid-maleki <maleki.anahid@gmail.com>
2023-01-26 13:11:44 +0330

    changing

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from flask import Flask, request, jsonify
 from deta import Deta
 
-app=Flask(__name__)
+
 
 # 2) initialize with a project key
 deta = Deta("a0ell3iu_1R3RhxZppEoGsHU2p36Ehhg22nM7xLFP")
 
 # 3) create and use as many DBs as you want!
 users = deta.Base("users")
+
+app=Flask(__name__)
 
 users.insert({
     "name": "Geordi",

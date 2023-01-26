a7ca8359d6b5cfb6cc7d38dfa7f4f75d37a2398f
Anahid-maleki <maleki.anahid@gmail.com>
2023-01-26 12:52:08 +0330

    pjkey

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from deta import Deta
 app=Flask(__name__)
 
 # 2) initialize with a project key
-deta = Deta("project key")
+deta = Deta("a0ell3iu_1R3RhxZppEoGsHU2p36Ehhg22nM7xLFP")
 
 # 3) create and use as many DBs as you want!
 users = deta.Base("users")

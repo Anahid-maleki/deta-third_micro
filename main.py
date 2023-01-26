287f9629f97b87428a82d2a7269c2ba7b2e86e2f
Anahid-maleki <maleki.anahid@gmail.com>
2023-01-26 15:14:12 +0330

    add to codeblock

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from deta import Deta
 deta = Deta("a0ell3iu_GHdFxTWpTdXYphdr9MF2Bro9fxE1W5kJ")  
 db = deta.Base("simple_db")
 
-db.put({"test": "test"})
 
 app=Flask(__name__)    
 @app.route('/', methods=['GET'])
 def home():
+    db.put({"test": "test"}) 
     return f"<html><body><h1>making database</h1></body></html>"

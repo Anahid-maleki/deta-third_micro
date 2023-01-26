4886e33790d1d99168d12b1f16abc20e74f9a9f2
Anahid-maleki <maleki.anahid@gmail.com>
2023-01-26 12:49:50 +0330

    user

--- main.py
+++ main.py
@@ -3,17 +3,11 @@ from deta import Deta
 
 app=Flask(__name__)
 
+# 2) initialize with a project key
+deta = Deta("project key")
 
-# Initialize with a Project Key
-# deta = Deta("a0ell3iu_AFmGiAybnBeSffX3WgzPBgd1EtKfkz6s")
-deta=Deta()
-
-# This how to connect to or create a database.
-db = deta.Base("first_db")
-
-# You can create as many as you want without additional charges.
-books = deta.Base("books")
-
+# 3) create and use as many DBs as you want!
+users = deta.Base("users")
 
 @app.route('/', methods=['GET'])
 def home():

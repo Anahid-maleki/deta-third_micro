9041b96f592264d969be8930f0022235abe39b0b
Anahid-maleki <maleki.anahid@gmail.com>
2023-01-26 10:02:23 +0330

    database

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-def app(event):
-    return "Hello, problem solver!"
+from flask import Flask, request, jsonify
+from deta import Deta
+
+app=Flask(__name__)
+# deta = Deta('myProjectKey') # configure your Deta project
+# db = deta.Base('simpleDB')  # access your DB
+
+@app.route('/', methods=['GET'])
+def home():
+    return f"<html><body><h1>Hello to you</h1></body></html>"

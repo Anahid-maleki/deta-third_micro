806fc921bd4ac2abd8e194b8bf156c24fd7ab47c
Anahid-maleki <maleki.anahid@gmail.com>
2023-01-26 13:25:31 +0330

    mn

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 from flask import Flask, request, jsonify
+
+
+
+
+# 1) pip install deta
 from deta import Deta
 
-
-
 # 2) initialize with a project key
-deta = Deta("a0ell3iu_1R3RhxZppEoGsHU2p36Ehhg22nM7xLFP")
+deta = Deta("project key")
 
 # 3) create and use as many DBs as you want!
-db = deta.Base("users")
+users = deta.Base("users")
 
-app=Flask(__name__)
+users.insert({
+    "name": "Geordi",
+    "title": "Chief Engineer"
+})
 
-# users.insert({
-#     "name": "Geordi",
-#     "title": "Chief Engineer"
-# })
+fetch_res = users.fetch({"name": "Geordi"})
 
-# fetch_res = users.fetch({"name": "Geordi"})
+for item in fetch_res.items:
+    users.delete(item["key"])
 
-# for item in fetch_res.items:
-#     users.delete(item["key"])
-
+app=Flask(__name__)    
 @app.route('/', methods=['GET'])
 def home():
     return f"<html><body><h1>Database</h1></body></html>"

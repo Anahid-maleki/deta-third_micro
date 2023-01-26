5182e4ac5c286b326f450eafdf195db8581581a5
Anahid-maleki <maleki.anahid@gmail.com>
2023-01-26 15:03:53 +0330

    discord

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from deta import Deta
 deta = Deta("a0ell3iu_GHdFxTWpTdXYphdr9MF2Bro9fxE1W5kJ")  
 db = deta.Base("simple_db")
 
-
+db.put({"test": "test"})
 
 app=Flask(__name__)    
 @app.route('/', methods=['GET'])

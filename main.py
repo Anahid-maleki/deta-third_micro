dbd510be1e59101766177e2792f25612c9cea164
Anahid-maleki <maleki.anahid@gmail.com>
2023-01-26 10:06:15 +0330

    database

--- main.py
+++ main.py
@@ -2,9 +2,33 @@ from flask import Flask, request, jsonify
 from deta import Deta
 
 app=Flask(__name__)
-# deta = Deta('myProjectKey') # configure your Deta project
-# db = deta.Base('simpleDB')  # access your DB
+
+
+
+deta = Deta('myProjectKey') # configure your Deta project
+db = deta.Base('simpleDB')  # access your DB
+app = Flask(__name__)
+
+{
+    "name": str,
+    "age": int,
+    "hometown": str
+}
+
+@app.route('/users', methods=["POST"])
+def create_user():
+    name = request.json.get("name")
+    age = request.json.get("age")
+    hometown = request.json.get("hometown")
+    
+    user = db.put({
+        "name": name,
+        "age": age,
+        "hometown": hometown
+    })
+
+    return jsonify(user, 201)
 
 @app.route('/', methods=['GET'])
 def home():
-    return f"<html><body><h1>Hello to you</h1></body></html>"
+    return f"<html><body><h1>Database</h1></body></html>"

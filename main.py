4742ce35b43b8f1fcf7e30681177c9ce2fdabd4d
Anahid-maleki <maleki.anahid@gmail.com>
2023-01-26 12:55:28 +0330

    ls

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ deta = Deta("a0ell3iu_1R3RhxZppEoGsHU2p36Ehhg22nM7xLFP")
 # 3) create and use as many DBs as you want!
 users = deta.Base("users")
 
+users.insert({
+    "name": "Geordi",
+    "title": "Chief Engineer"
+})
+
+fetch_res = users.fetch({"name": "Geordi"})
+
+for item in fetch_res.items:
+    users.delete(item["key"])
+
 @app.route('/', methods=['GET'])
 def home():
     return f"<html><body><h1>Database</h1></body></html>"

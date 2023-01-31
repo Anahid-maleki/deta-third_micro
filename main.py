1331049cc3bb1c455e1ac7d887e211b89b73563f
Anahid-maleki <maleki.anahid@gmail.com>
2023-01-31 13:33:22 +0330

    added static folder

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ deta = Deta("a0ell3iu_GHdFxTWpTdXYphdr9MF2Bro9fxE1W5kJ")
 db = deta.Base("simple_db")
 
 
-app=Flask(__name__)    
+app=Flask(__name__ , static_folder='static')    
 @app.route('/', methods=['GET'])
 def func():
     db.put({"name": "Anahid","key":"one"})
